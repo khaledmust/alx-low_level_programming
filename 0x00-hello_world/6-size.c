0e7c90025d653041f910e086f726bd2929a11f70
Khaled Mustafa <khaled.mustafa.elsayed@outlook.com>
2023-07-13 18:23:09 -0700

    Add 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	printf("Size of a char: %zd byte(s)\n", sizeof(char));
+	printf("Size of an int: %zd byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zd byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %zd byte(s)\n", sizeof(long long));
+	printf("Size of a float: %zd byte(s)\n", sizeof(float));
+
+	return (0);
+}
