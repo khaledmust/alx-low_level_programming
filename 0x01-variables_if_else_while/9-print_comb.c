f866e1b682870771faf54dc91fef32172f401d84
Khaled Mustafa <khaled.mustafa.elsayed@outlook.com>
2023-07-16 16:34:53 -0700

    Add 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: 0 program success.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i < 57)
+		{
+			putchar(44);
+		}
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
