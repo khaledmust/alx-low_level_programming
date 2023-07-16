f2f3aeecece944e37324e3819afe12ea5b2560d7
Khaled Mustafa <khaled.mustafa.elsayed@outlook.com>
2023-07-16 13:40:21 -0700

    Add 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -  Entry point of the program.
+ *
+ * Return: 0 if the program is success.
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+return (0);
+}
