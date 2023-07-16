f0e3dfc3301c0670e986ce40d3a52f48a3c4fa33
Khaled Mustafa <khaled.mustafa.elsayed@outlook.com>
2023-07-16 16:27:28 -0700

    Add 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: 0 program success.
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	for (j = 97; j <= 102; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+
+	return (0);
+}
