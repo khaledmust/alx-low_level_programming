cec5609818cb7cb9c1755ce7c40e30f9ff24d88c
Khaled Mustafa <khaled.mustafa.elsayed@outlook.com>
2023-07-16 16:22:01 -0700

    Add 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: 0 program is success.
+ */
+int main(void)
+{
+	int i;
+	
+	for (i = 122; i >= 97; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
