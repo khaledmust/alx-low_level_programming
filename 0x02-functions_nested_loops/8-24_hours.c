d1812eb3af97a24fbb4470bf905ef0eadbd8adf4
Khaled Mustafa <khaled.mustafa.elsayed@outlook.com>
2023-07-19 19:03:57 -0700

    Add 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ */
+void jack_bauer(void)
+{
+	int hours, minutes, tmp;
+
+	for (hours = 0; hours < 24; hours++)
+	{
+		for (minutes = 0; minutes < 60; minutes++)
+		{
+			if (hours < 10)
+			{
+				_putchar(hours + 48);
+				_putchar(58);
+			}
+			else
+			{
+				tmp = hours / 10;
+				_putchar(tmp + 48);
+				tmp = hours % 10;
+				_putchar(tmp + 48);
+				_putchar(58);
+			}
+			if (minutes < 10)
+			{
+				_putchar(minutes + 48);
+				_putchar('\n');
+			}
+			else
+			{
+				tmp = minutes / 10;
+				_putchar(tmp + 48);
+				tmp = minutes % 10;
+				_putchar(tmp + 48);
+				_putchar('\n');
+			}
+		}
+	}
+}
