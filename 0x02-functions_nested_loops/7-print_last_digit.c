306c192fc6b08335dbd7b5f5d3fca5b0dd52c407
Khaled Mustafa <khaled.mustafa.elsayed@outlook.com>
2023-07-19 18:28:14 -0700

    Fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,17 +11,14 @@ int print_last_digit(int j)
 {
 	int i;
 
-	i = 0;
-	if (j < 0)
+	i = j % 10;
+	if (i < 0)
 	{
-		i = j * -1;
-		i = i % 10;
-		_putchar(i + 48);
+		_putchar(-i + 48);
 		return (-i);
 	}
 	else
 	{
-		i = j % 10;
 		_putchar(i + 48);
 		return (i);
 	}
