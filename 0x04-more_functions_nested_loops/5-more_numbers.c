9199cffb73ad7c732753c1a0fe236802ad18680c
Khaled Mustafa <khaled.mustafa.elsayed@outlook.com>
2023-07-20 18:43:48 -0700

    Fix logic

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,11 +7,15 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i < 14; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + 48);
+			if (j >= 10)
+			{
+				_putchar(j / 10 + 48);
+			}
+			_putchar(j % 10 + 48);
 		}
 		_putchar('\n');
 	}
