c01f71a47bdd06ecf27c460eb925fc4199007c7e
Khaled Mustafa <khaled.mustafa.elsayed@outlook.com>
2023-07-21 02:38:22 -0700

    Add 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long input = 612852475143;
+
+	int x;
+
+	x = 2;
+
+	while (input > 1)
+	{
+		while (input % x == 0)
+		{
+			input /= x;
+		}
+		x += 1;
+	}
+	printf("%d\n", x);
+
+	return (0);
+}
