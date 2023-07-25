853d6c2ed3328cb6fdc12dd76c345a7bbdda3107
Khaled Mustafa <khaled.mustafa.elsayed@outlook.com>
2023-07-24 18:18:45 -0700

    Add 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ *
+ * @s: Address of the string
+ */
+void print_rev(char *s)
+{
+	int i, j;
+
+	i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+
+	for (j = i; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+	_putchar('\n');
+
+}
