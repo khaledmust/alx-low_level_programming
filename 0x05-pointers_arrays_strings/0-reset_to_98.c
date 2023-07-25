9c45f80b3c56ccf5801405a857999a310ec544f0
Khaled Mustafa <khaled.mustafa.elsayed@outlook.com>
2023-07-24 17:40:20 -0700

    Add 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and updates the value
+ * it points to to 98
+ *
+ * @n: Address of the variable to be changed.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
