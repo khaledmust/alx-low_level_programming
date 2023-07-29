ef48d06d74859ad704f6ab682eda9561417a6d73
Khaled Mustafa <khaled.mustafa.elsayed@outlook.com>
2023-07-28 18:47:32 -0700

    Fix function logic

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,12 +10,18 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	if (s1[0] == s2[0])
+	int i = 0, diff = 0;
+
+	while (diff == 0)
 	{
-		return (0);
+		if ((s1[i] == '\0') && (s2[i] == '\0'))
+		{
+			break;
+		}
+
+		diff = s1[i] - s2[i];
+		i++;
 	}
-	else
-	{
-		return (s1[0] - s2[0]);
-	}
+
+	return (diff);
 }
