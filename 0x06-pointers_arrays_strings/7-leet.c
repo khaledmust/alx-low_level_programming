26e81cc7b41ed229d787ff0681cd362c28aa687c
Khaled Mustafa <khaled.mustafa.elsayed@outlook.com>
2023-07-28 19:49:21 -0700

    Add 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+char *leet(char *s)
+{
+	int i = 0, j;
+	int lower_case_letters[] = {97, 101, 111, 116, 108};
+	int upper_case_letters[] = {65, 69, 79, 84, 76};
+	int numbers[] = {52, 51, 48, 55, 49};
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == lower_case_letters[j]
+			    || s[i] == upper_case_letters[j])
+			{
+				s[i] = numbers[j];
+				break;
+			}
+		}
+		i++;
+	}
+
+	return (s);
+}
