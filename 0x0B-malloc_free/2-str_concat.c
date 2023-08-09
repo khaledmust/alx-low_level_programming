cb0413e28ab7a163657dbbc09971719af3927124
Khaled Mustafa <khaled.mustafa.elsayed@outlook.com>
2023-08-09 02:11:32 -0700

    Apply fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,17 +14,10 @@ char *str_concat(char *s1, char *s2)
 	int i, j, k;
 	char *strout;
 
-	if (s1 == NULL && s2 == NULL)
-	{
-		return (NULL);
-	}
-	else if (s1 == NULL || s2 == NULL)
-	{
-		if (s1 == NULL)
-			s1 = "";
-		else if (s2 == NULL)
-			s2 = "";
-	}
+	if (s1 == NULL)
+		s1 = "";
+	else if (s2 == NULL)
+		s2 = "";
 
 	i = 0;
 	while (s1[i] != '\0')
