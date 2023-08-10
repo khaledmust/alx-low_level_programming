90f9cb99b670b45431e552f53ed60559502a200e
Khaled Mustafa <khaled.mustafa.elsayed@outlook.com>
2023-08-09 19:23:40 -0700

    Add 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * @argc: Number of command line arguments.
+ * @argv: Array containing the program command line arugments.
+ *
+ * Return: 0 - Success
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
